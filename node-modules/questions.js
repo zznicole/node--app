4c7e6858fa6ccfc3b391de8f6c8cf1535b943581
zznicole <zznicole@hotmail.com>
2020-09-02 15:03:00 +0200

    create a module

--- node-modules/questions.js
+++ node-modules/questions.js
@@ -1,28 +1,29 @@
-const readline = require ( "readline");
+// const readline = require ( "readline");
+const collectAnswers = require("../lib/collectAnswers");
 const questions = [
     "What is your name?",
     "Where do you live?",
     "What are you going to do with Node js?"
 ];
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
-const collectAnswers = (questions, done) => {
-    const answers = [];
-    const [firstQuestion] = questions;
-    const questionAnswered = answer => {
-        answers.push(answer);
-        if (answers.length < questions.length) {
-            rl.question(questions[answers.length], questionAnswered);
-        } else {
-            done(answers);
-        }
-    }
-    rl.question(firstQuestion, questionAnswered);
-};
+// const collectAnswers = (questions, done) => {
+//     const answers = [];
+//     const [firstQuestion] = questions;
+//     const questionAnswered = answer => {
+//         answers.push(answer);
+//         if (answers.length < questions.length) {
+//             rl.question(questions[answers.length], questionAnswered);
+//         } else {
+//             done(answers);
+//         }
+//     }
+//     rl.question(firstQuestion, questionAnswered);
+// };
 
 collectAnswers(questions, answers => {
     console.log("Thank you for your answers. ");
